04bd150a9aac39bede3a3320fd26a51938a6efaf
SamuelSamy <fatu.samuel24@gmail.com>
2021-11-29 01:39:18 +0200

    Implemented Enum Classes

---
+++ modules/moderation/package/enums.py
@@ -0,0 +1,17 @@
+from enum import Enum
+
+class ModFormat(Enum):
+    next_id     = "next-case-id"     
+    logs        = "logs"
+    mod_logs    = "mod-logs"
+    channel     = "logs-channel"
+
+
+class CaseFormat(Enum):
+    case_id     = "case_id"
+    _type       = "type"
+    reason      = "reason"
+    time        = "time"
+    moderator   = "moderator"
+    duration    = "duration"
+    
