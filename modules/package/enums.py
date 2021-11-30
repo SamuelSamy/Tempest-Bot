53709ebad09d8674e2d9e96dc522a544121e6cd8
SamuelSamy <fatu.samuel24@gmail.com>
2021-12-01 01:42:11 +0200

    Added Global Enums / Exceptions / Utilities

--- modules/package/enums.py
+++ modules/package/enums.py
@@ -2,20 +2,21 @@ from enum import Enum
 
 
 class Settings(Enum):
-    slowmode_channels    = "slowmode_channels" # [{}]
-    lockdown_channels    = "lockdown_channels" # []
+    slowmode_channels    = "slowmode-channels" # [{}]
+    lockdown_channels    = "lockdown-channels" # []
     
-    lock_role            = "lock_role" # int
+    lock_role            = "lock-role" # int
     
-    scam_logs            = "scam_logs" # int
+    scam_logs            = "scam-logs" # int
 
-    muted_role           = "muted_role" # int
+    muted_role           = "muted-role" # int
     
-    helpers              = "helpers" # []
-    helper_logs          = "helper_logs" # int
-    helper_roles         = "helper_roles" # []
+    helper_logs          = "helper-logs" # int
+    helper_roles         = "helper-roles" # []
 
-    suggestions_channels = "suggestions_channels" # []
+    suggestions_channels = "suggestions-channels" # []
+
+    staff_roles          = "staff-roles" # []
 
 
 class Colors(Enum):
@@ -27,3 +28,9 @@ class Colors(Enum):
     white   = 0xCCCCCC
     black   = 0xEFF7FA
 
+
+class Emotes(Enum):
+    red_tick    = "<:redTick:915333183715815444>"
+    green_tick  = "<:greenTick:915333220726374461>"
+    loading     = "<a:loading:915333280633598052>"
+    orange_flag = "<:OrangeFlag:915333566366367784>"
