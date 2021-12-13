3a94d3d17bc99d0b08aa5944ac22f299f32f1345
SamuelSamy <fatu.samuel24@gmail.com>
2021-12-14 00:02:40 +0200

    New Exception

--- modules/package/exceptions.py
+++ modules/package/exceptions.py
@@ -59,3 +59,14 @@ class CaseException(Exception):
 
     def __str__(self):
         return self.__message
+
+
+class LevelingError(Exception):
+
+    def __init__(self, message):
+        self.__message = message
+
+    def __str__(self):
+        return self.__message
+
+
