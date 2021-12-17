4d1ca70f9ff5e0d17054482e2386d6cfb43598c3
SamuelSamy <fatu.samuel24@gmail.com>
2021-12-17 20:04:38 +0200

    Better error listener

--- modules/error_listener.py
+++ modules/error_listener.py
@@ -31,7 +31,6 @@ class CommandErrorHandler(commands.Cog):
         # Allows us to check for original exceptions raised and sent to CommandInvokeError.
         # If nothing is found. We keep the exception passed to on_command_error.
         error = getattr(error, 'original', error)
-
         # Anything in ignored will return and prevent anything happening.
         if isinstance(error, ignored):
             return
@@ -47,7 +46,9 @@ class CommandErrorHandler(commands.Cog):
                 pass
         
         elif isinstance(error, commands.MissingPermissions) or isinstance(error, commands.CheckFailure):
-            await ctx.reply(f'{Emotes.no_entry.value}You can not use that comamnd!')
+            
+            if str(error) != "You do not own this bot.":
+                await ctx.reply(f'{Emotes.no_entry.value}You can not use that comamnd!')
         
         elif isinstance(error, commands.RoleNotFound):
             await ctx.reply(f"{Emotes.not_found.value } The specified role does not exist!")
@@ -56,16 +57,38 @@ class CommandErrorHandler(commands.Cog):
             await ctx.reply(f"{Emotes.not_found.value } The specified channel does not exist!")
 
         elif isinstance(error, commands.MissingRequiredArgument):
-            await ctx.reply(f"{Emotes.not_found.value} Missing required arguments!\nUse `{ctx.prefix}help {ctx.command}` for aditional help!")
+            await ctx.reply(f"{Emotes.not_found.value} Missing required arguments!")
 
         elif isinstance(error, commands.BadUnionArgument):
-            await ctx.reply(f"{Emotes.not_found.value} Invalid argument!\nUse `{ctx.prefix}help {ctx.command}` for aditional help!")
+            await ctx.reply(f"{Emotes.not_found.value} Invalid arguments!")
         
+        elif isinstance(error, commands.UserNotFound):
+            await ctx.reply(f"{Emotes.not_found.value} The specified user is not in this server or does not exist!")
+
         else:
             # All other Errors not returned come here. And we can just print the default TraceBack.
             await ctx.reply(f'{Emotes.wrong.value} Something went wrong.')
             
-            print(f'Ignoring exception in command {ctx.command}:', file=sys.stderr)
+
+            error_channel = self.bot.get_channel(921429366007816212)
+            
+            message  = f'The command: `{ctx.command}` raised an error\n'
+            message += f"Error type: {type(error)}\n"
+            message += f"Short description: {error}\n"
+            message += f"Error message:\n"
+
+            lines = traceback.format_exception(type(error), error, error.__traceback__)
+            for line in lines:
+                message += line
+
+            message = [message[i: i + 1900] for i in range(0, len(message), 1900)]
+
+            for _message in message:
+                await error_channel.send(f"```\n{_message}\n```")
+
+            await error_channel.send("``` ```")
+
+            print('Ignoring exception in command {}:'.format(ctx.command), file=sys.stderr)
             traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
 
 
